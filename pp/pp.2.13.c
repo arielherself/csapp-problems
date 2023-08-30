cd6710acad27abbe2849c87e45538317840b46b6
arielherself <ariel@arielherself.xyz>
2023-08-30 11:52:36 +0800

    add pp.2.13

---
+++ pp/pp.2.13.c
@@ -0,0 +1,33 @@
+//
+// Created by user on 8/30/23.
+//
+#include "include/common.h"
+
+int bis(int x, int m) {
+    return x | m;
+}
+
+int bic(int x, int m) {
+    return (x ^ m) & x;
+}
+
+int bool_or(int x, int y) {
+    int result = bis(x, y);
+    return result;
+}
+
+int bool_xor(int x, int y) {
+    int result = bis(bic(x, y), bic(y, x));
+    return result;
+}
+
+int main() {
+    int a = 0x01101011, b = 0x10011100;
+    show_bytes(&a, 4);
+    show_bytes(&b, 4);
+    int c = bool_or(a, b);
+    show_bytes(&c, 4);
+    c = bool_xor(a, b);
+    show_bytes(&c, 4);
+    return 0;
+}
